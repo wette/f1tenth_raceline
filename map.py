742f7d57aedbeda296806ee2bcd90998ecfced71
Philip Wette <philip.wette@fh-bielefeld.de>
2024-01-01 17:44:03 +0100

    first working version

---
+++ map.py
@@ -0,0 +1,82 @@
+
+class Map:
+    def __init__(self, image_file: str, origin: list, resolution: float):
+        self.__map = self.parse_image(image_file)
+
+        self.__origin_x = int(origin[0] / resolution * -1.0)
+        self.__origin_y = int(origin[1] / resolution * -1.0)
+
+        self.binarize_image()
+        self.region_growing()
+
+    #override []-operator
+    def __getitem__(self, key):
+        return self.__map[key]
+    
+    def get_pixel_map(self):
+        return self.__map
+
+    def parse_image(self, image_file: str):
+        """Return a raster of integers from a PGM as a list of lists.
+           from https://stackoverflow.com/questions/35723865/read-a-pgm-file-in-python""" 
+        pgmf = open(image_file, "r", encoding="latin-1")
+        firstline = pgmf.readline()
+        assert firstline == 'P5\n'
+        (width, height) = [int(i) for i in pgmf.readline().split()]
+        depth = int(pgmf.readline())
+        assert depth <= 255
+
+        raster = []
+        for y in range(height):
+            row = []
+            for x in range(width):
+                row.append(ord(pgmf.read(1)))
+            raster.append(row)
+        return raster
+    
+    def binarize_image(self):
+        """use the thresholds to create a 0/1 image showing freespace as 0"""
+        threshold_free = 220
+        for x in range(0, len(self.__map)):
+            for y in range(0, len(self.__map[0])):
+                if self.__map[x][y] >= threshold_free:
+                    self.__map[x][y] = 0
+                else:
+                    self.__map[x][y] = 1
+
+    def region_growing(self):
+        """use region growing to remove unreachable parts of the map"""
+        visited_marker = 256
+
+        #return neighbors in 4-neighborhood (up, down, left, right)
+        def neighbors(x, y, max_x, max_y):
+            n = []
+            n.append( (x-1, y) )
+            n.append( (x+1, y) )
+            n.append( (x, y+1) )
+            n.append( (x, y-1) )
+
+            #filter out corners!
+            for i in range(len(n)-1,0, -1):
+                if n[i][0] < 0 or n[i][0] > max_x or n[i][1] < 0 or n[i][1] > max_y:
+                    n.remove( n[i] )
+
+            return n
+
+        #BFS
+        queue = [ (self.__origin_x, self.__origin_y) ]
+        self.__map[self.__origin_x][self.__origin_y] = visited_marker
+        while len(queue) > 0:
+            cur = queue.pop(0)
+            for x,y in neighbors(cur[0], cur[1], len(self.__map)-1, len(self.__map[0])-1):
+                if self.__map[x][y] == 0:
+                    self.__map[x][y] = visited_marker
+                    queue.append( (x,y) )
+
+        #flip back visited_marker to 0
+        for x in range(len(self.__map)):
+            for y in range(len(self.__map[0])):
+                if self.__map[x][y] == visited_marker:
+                    self.__map[x][y] = 0
+                else:
+                    self.__map[x][y] = 1
---
+++ trajectory.py
@@ -0,0 +1,108 @@
+import random
+import math
+import pycubicspline.pycubicspline as pyspline
+
+class Trajectory:
+    def __init__(self, x: list, y: list, curvature=None):
+        self.x = x
+        self.y = y
+        self.curvature = curvature
+        self.length = None
+        self.laptime = None
+
+        if self.curvature is None:
+            self.x, self.y, _, self.curvature, _ = pyspline.calc_2d_spline_interpolation(self.x, self.y, num=len(self.y))
+    
+    def get_length(self) -> int:
+        if self.length is not None:
+            return self.length
+        
+        #compute length of spline
+        self.length = 0.0
+        for i in range(0, len(self.x)-1):
+            x2 = self.x[i+1] - self.x[i]
+            x2 *= x2
+            y2 = self.y[i+1] - self.y[i]
+            y2 *= y2
+            self.length += math.sqrt( x2 + y2 )
+
+        #add length from last back to first.
+        x2 = self.x[-1] - self.x[0]
+        x2 *= x2
+        y2 = self.y[-1] - self.y[0]
+        y2 *= y2
+        self.length += math.sqrt( x2 + y2 )
+
+        return self.length
+    
+    def get_laptime(self) -> int:
+        if self.laptime is not None:
+            return self.laptime
+        
+        haftreibung = 0.4
+        
+        #compute length of spline and divide by max. curvature speed --> time
+        self.laptime = 0.0
+        for i in range(0, len(self.x)-1):
+            x2 = self.x[i+1] - self.x[i]
+            x2 *= x2
+            y2 = self.y[i+1] - self.y[i]
+            y2 *= y2
+
+            length = math.sqrt( x2 + y2 )
+            max_speed = math.sqrt(haftreibung/(1.0-haftreibung) * 9.8 * abs(1.0/self.curvature[i]))
+            if max_speed < 0.0001:
+                continue
+            self.laptime += length/max_speed
+
+        #add length from last back to first.
+        x2 = self.x[-1] - self.x[0]
+        x2 *= x2
+        y2 = self.y[-1] - self.y[0]
+        y2 *= y2
+        
+        length = math.sqrt( x2 + y2 )
+        max_speed = math.sqrt(haftreibung/(1.0-haftreibung) * 9.8 * abs(1.0/self.curvature[i]))
+        self.laptime += length/max_speed
+
+        return self.laptime
+
+
+    def random_changes(self, max_change_px: float, num_changes: int, map: list):
+        """randomly change the trajectory"""
+        for i in range(num_changes):
+            idx = random.randint(0, len(self.x)-1)
+            x = self.x[idx]
+            y = self.y[idx]
+            #test different changes - keep the first one which is in free space
+            while True:
+                change_x = max_change_px - random.random()*max_change_px*2.0
+                change_y = max_change_px - random.random()*max_change_px*2.0
+
+                self.x[idx] = x + change_x
+                self.y[idx] = y + change_y
+
+                if map[ int(self.y[idx]) ][ int(self.x[idx]) ] == 0.0:
+                    break
+
+        #apply spline smoothing
+        self.x, self.y, _, self.curvature, _ = pyspline.calc_2d_spline_interpolation(self.x, self.y, num=len(self.y))
+        self.length = None
+
+    def random_combination(self, other_trajectory: 'Trajectory'):
+        start_idx = random.randint(0, len(self.x)-1)
+        end_idx   = random.randint(start_idx, len(self.x)-1)
+
+        for i in range(start_idx, end_idx):
+            self.x[i] = other_trajectory.x[i]
+            self.y[i] = other_trajectory.y[i]
+
+        #apply spline smoothing
+        self.x, self.y, _, self.curvature, _ = pyspline.calc_2d_spline_interpolation(self.x, self.y, num=len(self.y))
+        self.length = None
+
+    def copy(self) -> 'Trajectory':
+        if self.curvature is not None:
+            return Trajectory(self.x[:], self.y[:], self.curvature[:])
+        else:
+            return Trajectory(self.x[:], self.y[:])
